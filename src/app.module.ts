1f69aa8057df57db6d3b98bf145c7382d04e8156
Rost Myshkin <rostmyshkin@gmail.com>
2020-02-01 17:56:01 -0800

    comment module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
 import { UserModule } from "./user/user.module";
 import { ReviewsModule } from "./review/review.module";
+import { CommentModule } from './comment/comment.module';
 
 @Module({
-  imports: [UserModule, ReviewsModule],
+  imports: [UserModule, ReviewsModule, CommentModule],
   controllers: [],
   providers: []
 })
