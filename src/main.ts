27492d548c830bc4178eb8f2776751074aee9b76
Rost Myshkin <rostmyshkin@gmail.com>
2020-02-01 17:59:28 -0800

    added .env file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { Logger } from "@nestjs/common";
+import "dotenv/config";
 
+// local dev port
+const port = process.env.PORT || 3000;
+
+/**
+ * API entry point
+ */
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  Logger.log(`Server running on port http://localhost:${port}`, "Bootstrap");
 }
 bootstrap();
