7b1e644c1e5bf85ac4a64fe99937a2355dba6a66
Rost Myshkin <rostmyshkin@gmail.com>
2020-02-01 17:36:06 -0800

    users module with base setup

---
+++ src/user/user.module.ts
@@ -0,0 +1,15 @@
+import { Module } from "@nestjs/common";
+
+import { UserController } from "./user.controller";
+import { UserService } from "./user.service";
+import { UserEntity } from "./user.entity";
+
+/**
+ * User Module
+ */
+@Module({
+  imports: [TypeOrmModule.forFeature([UserEntity])],
+  controllers: [UserController],
+  providers: [UserService]
+})
+export class UserModule {}
