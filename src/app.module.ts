6db47e5d835cfc3e5dfe498b9b4b311c717cbe32
Rost Myshkin <rostmyshkin@gmail.com>
2020-02-01 22:42:56 -0800

    added main module file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,16 @@
-import { Module } from "@nestjs/common";
-import { TypeOrmModule } from "@nestjs/typeorm";
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { UserModule } from "./user/user.module";
-import { ReviewsModule } from "./review/review.module";
-import { CommentModule } from "./comment/comment.module";
+import { UserModule } from './user/user.module';
+import { CommentModule } from './comment/comment.module';
 import { PostModule } from './post/post.module';
 
 /**
  * Root Module
  */
 @Module({
-  imports: [TypeOrmModule.forRoot(), UserModule, ReviewsModule, CommentModule, PostModule],
+  imports: [TypeOrmModule.forRoot(), UserModule, CommentModule, PostModule],
   controllers: [],
-  providers: []
+  providers: [],
 })
 export class AppModule {}
