784224123f68fbd8a9c0ffaf370e9d9678cc255b
Rost Myshkin <rostmyshkin@gmail.com>
2020-02-01 18:08:16 -0800

    imported typeorm dependency

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+
 import { UserModule } from "./user/user.module";
 import { ReviewsModule } from "./review/review.module";
-import { CommentModule } from './comment/comment.module';
+import { CommentModule } from "./comment/comment.module";
 
+/**
+ * Root Module
+ */
 @Module({
-  imports: [UserModule, ReviewsModule, CommentModule],
+  imports: [TypeOrmModule.forRoot(), UserModule, ReviewsModule, CommentModule],
   controllers: [],
   providers: []
 })
