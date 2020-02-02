7b8f1bdc73327ff0a01d13e1d8ecb119988b8b0c
Rost Myshkin <rostmyshkin@gmail.com>
2020-02-01 18:05:28 -0800

    installed typeorm, pg and dotenv; configured orm config file

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { Logger } from "@nestjs/common";
 import "dotenv/config";
+import "reflect-metadata";
 
 // local dev port
 const port = process.env.PORT || 3000;
