2b2a872582514898a7dce19427765c36c6c6bae3
Rost Myshkin <rostmyshkin@gmail.com>
2020-02-01 19:22:24 -0800

    changed port to process port in main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
-import { Logger } from "@nestjs/common";
-import "dotenv/config";
-import "reflect-metadata";
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import 'dotenv/config';
+import 'reflect-metadata';
 
 // local dev port
 const port = process.env.PORT || 3000;
@@ -12,7 +12,7 @@ const port = process.env.PORT || 3000;
  */
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-  Logger.log(`Server running on port http://localhost:${port}`, "Bootstrap");
+  await app.listen(port);
+  Logger.log(`Server running on port http://localhost:${port}`, 'Bootstrap');
 }
 bootstrap();
