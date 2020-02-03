c58236547cec6631df01be8304481745297b8c22
Rost Myshkin <rostmyshkin@gmail.com>
2020-02-02 21:58:10 -0800

    updated docker file command; updated posts remove method to fix fk problem

--- src/comment/comment.module.ts
+++ src/comment/comment.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { CommentController } from './comment.controller';
 import { CommentService } from './comment.service';
 import { CommentEntity } from './comment.entity';
@@ -6,7 +6,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostModule } from 'src/post/post.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CommentEntity]), PostModule],
+  imports: [
+    TypeOrmModule.forFeature([CommentEntity]),
+    forwardRef(() => PostModule),
+  ],
   controllers: [CommentController],
   providers: [CommentService],
   exports: [CommentService],
