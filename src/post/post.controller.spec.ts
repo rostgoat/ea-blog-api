d0bfd2c39ba90c7aa0a981e6cf71adba447080fc
Rost Myshkin <rostmyshkin@gmail.com>
2020-02-03 00:41:59 -0800

    pre test setup for post service and controller

--- src/post/post.controller.spec.ts
+++ src/post/post.controller.spec.ts
@@ -1,18 +1,48 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PostController } from './post.controller';
+import { UserService } from '../user/user.service';
+import { PostService } from './post.service';
+import { Repository } from 'typeorm';
+import { PostEntity } from './post.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
+class UserServiceMock extends UserService {}
+class PostServiceMock extends PostService {}
+
+/**
+ * Post Controller Unit Test
+ */
 describe('Post Controller', () => {
-  let controller: PostController;
+  let postController: PostController;
+  let postService: PostService;
+  let repo: Repository<PostEntity>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PostController],
+      providers: [
+        {
+          provide: PostService,
+          useClass: PostServiceMock,
+        },
+        {
+          provide: PostService,
+          useValue: PostServiceMock,
+        },
+        PostService,
+        {
+          provide: getRepositoryToken(PostEntity),
+          useClass: Repository,
+        },
+      ],
     }).compile();
 
-    controller = module.get<PostController>(PostController);
+    postService = module.get<PostService>(PostService);
+    repo = module.get<Repository<PostEntity>>(getRepositoryToken(PostEntity));
+    postController = module.get<PostController>(PostController);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(postController).toBeDefined();
   });
 });
--- src/post/post.service.spec.ts
+++ src/post/post.service.spec.ts
@@ -4,6 +4,11 @@ import { Repository } from 'typeorm';
 import { PostEntity } from './post.entity';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import * as faker from 'faker';
+import { UserService } from '../user/user.service';
+import { CommentService } from '../comment/comment.service';
+
+class UserServiceMock extends UserService {}
+class CommentServiceMock extends CommentService {}
 
 describe('PostService', () => {
   let service: PostService;
@@ -14,16 +19,21 @@ describe('PostService', () => {
       providers: [
         PostService,
         {
-          // how you provide the injection token in a test instance
           provide: getRepositoryToken(PostEntity),
-          // as a class value, Repository needs no generics
           useClass: Repository,
+        },
+        {
+          provide: UserService,
+          useValue: UserServiceMock,
+        },
+        {
+          provide: CommentService,
+          useValue: CommentServiceMock,
         },
       ],
     }).compile();
 
     service = module.get<PostService>(PostService);
-    // Save the instance of the repository and set the correct generics
     repo = module.get<Repository<PostEntity>>(getRepositoryToken(PostEntity));
   });
 
