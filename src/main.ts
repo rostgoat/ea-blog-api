fd871395039093ed49cdf8beb7fbaf98f1913b09
Rost Myshkin <rostmyshkin@gmail.com>
2020-02-04 10:47:09 -0800

    added swagger implementation in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { Logger } from '@nestjs/common';
 import 'dotenv/config';
 import 'reflect-metadata';
@@ -12,6 +13,19 @@ const port = process.env.PORT || 3000;
  */
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger docs options
+  const options = new DocumentBuilder()
+    .setTitle('EA API')
+    .setDescription('The EA Blog API')
+    .setVersion('1.0')
+    .addTag('ea-blog')
+    .build();
+
+  // connecting swagger to the app
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(port);
   Logger.log(`Server running on port http://localhost:${port}`, 'Bootstrap');
 }
