84132811e0dd1bfb0d1c010811d51a2a1775f39f
Rost Myshkin <rostmyshkin@gmail.com>
2020-02-04 11:27:18 -0800

    same thing for posts and comments as above

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { Logger } from '@nestjs/common';
 import 'dotenv/config';
 import 'reflect-metadata';
 import { UserModule } from './user/user.module';
-import { usersOptions } from './swagger/swagger';
+import { usersOptions, postsOptions, commentsOptions } from './swagger/swagger';
 
 // local dev port
 const port = process.env.PORT || 3000;
@@ -16,12 +16,25 @@ const port = process.env.PORT || 3000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // connecting swagger to the app
-  const document = SwaggerModule.createDocument(app, usersOptions, {
+  // connecting user docs to swagger
+  const userDocument = SwaggerModule.createDocument(app, usersOptions, {
     include: [UserModule],
   });
 
-  SwaggerModule.setup('api/users', app, document);
+  // connecting blog posts docs to swagger
+  const postDocument = SwaggerModule.createDocument(app, postsOptions, {
+    include: [UserModule],
+  });
+
+  // connecting comments docs to swagger
+  const commentDocument = SwaggerModule.createDocument(app, commentsOptions, {
+    include: [UserModule],
+  });
+
+  // defining docs routes
+  SwaggerModule.setup('api/users', app, userDocument);
+  SwaggerModule.setup('api/posts', app, postDocument);
+  SwaggerModule.setup('api/comments', app, commentDocument);
 
   await app.listen(port);
   Logger.log(`Server running on port http://localhost:${port}`, 'Bootstrap');
--- src/swagger/swagger.ts
+++ src/swagger/swagger.ts
@@ -1,8 +1,28 @@
 import { DocumentBuilder } from '@nestjs/swagger';
 
-// Swagger docs options for user API methods
+/**
+ * This file contains all the Swagger API docs for every schema type
+ */
+const DESCRIPTION = 'The EA Blog API';
+const VERSION = '1.0.0';
+
+// User API methods
 export const usersOptions = new DocumentBuilder()
   .setTitle('Users')
-  .setDescription('The EA Blog API')
-  .setVersion('1.0')
+  .setDescription(DESCRIPTION)
+  .setVersion(VERSION)
+  .build();
+
+// Blog Posts API methods
+export const postsOptions = new DocumentBuilder()
+  .setTitle('Posts')
+  .setDescription(DESCRIPTION)
+  .setVersion(VERSION)
+  .build();
+
+// Comments API methods
+export const commentsOptions = new DocumentBuilder()
+  .setTitle('Comments')
+  .setDescription(DESCRIPTION)
+  .setVersion(VERSION)
   .build();
