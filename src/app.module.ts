d1462d3a72ea1f3c49717eab2bc0320d9f755156
Rost Myshkin <rostmyshkin@gmail.com>
2020-02-25 13:32:59 -0800

    simple server connection established

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 import { CommentModule } from './comment/comment.module';
 import { PostModule } from './post/post.module';
+import { AppGateway } from './app.gateway';
 
 /**
  * Root Module
@@ -11,5 +12,6 @@ import { PostModule } from './post/post.module';
 @Module({
   imports: [TypeOrmModule.forRoot(), UserModule, CommentModule, PostModule],
   exports: [UserModule, CommentModule, PostModule],
+  providers: [AppGateway],
 })
 export class AppModule {}
