cfb5c81190c5730db37df71e5de6f779b5d8f65b
Rost Myshkin <rostmyshkin@gmail.com>
2020-02-27 00:09:01 -0800

    created auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,13 @@ import { UserModule } from './user/user.module';
 import { CommentModule } from './comment/comment.module';
 import { PostModule } from './post/post.module';
 import { AppGateway } from './app.gateway';
+import { AuthModule } from './auth/auth.module';
 
 /**
  * Root Module
  */
 @Module({
-  imports: [TypeOrmModule.forRoot(), UserModule, CommentModule, PostModule],
+  imports: [TypeOrmModule.forRoot(), UserModule, CommentModule, PostModule, AuthModule],
   exports: [UserModule, CommentModule, PostModule],
   providers: [],
 })
