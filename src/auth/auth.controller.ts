23ccd73826dbe0b8535474025d8c847f060c789a
Rost Myshkin <rostmyshkin@gmail.com>
2020-02-27 18:08:43 -0800

    jwt env vars; email to username in multiple files

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { AuthService, RegistrationStatus } from './auth.service';
+import { UserCreateDTO } from 'src/user/user.create.dto';
+import { UserLoginDTO } from 'src/user/user.login.dto';
+import { LoginStatus } from './interfaces/login-status.interface';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  public async register(
+    @Body() createUserDto: UserCreateDTO,
+  ): Promise<RegistrationStatus> {
+    const result: RegistrationStatus = await this.authService.register(
+      createUserDto,
+    );
+    if (!result.success) {
+      throw new HttpException(result.message, HttpStatus.BAD_REQUEST);
+    }
+    return result;
+  }
+
+  @Post('login') public async login(@Body() loginUserDto: UserLoginDTO): Promise<LoginStatus> {
+    return await this.authService.login(loginUserDto);
+  }
+}
