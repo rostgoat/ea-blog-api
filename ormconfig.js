ed80dbfe6dacefff66bb3ac5d1dda16701139712
Rost Myshkin <rostmyshkin@gmail.com>
2020-03-24 20:15:46 -0700

    orm prod configs

--- ormconfig.js
+++ ormconfig.js
@@ -1,14 +1,17 @@
 const env = require('dotenv')
 env.config()
 
+const production = process.env.NODE_ENV === 'production';
+
 module.exports = {
   name: 'default',
-  type: process.env.EA_DATABASE_TYPE,
-  host: process.env.EA_DATABASE_HOST,
+  url: production ? process.env.EA_PROD_DATABASE_URL : '',
+  type: production ? process.env.EA_PROD_DATABASE_TYPE : process.env.EA_DATABASE_TYPE,
+  host: production ? process.env.EA_PROD_DATABASE_HOST : process.env.EA_DATABASE_HOST,
   port: 5432,
-  username: process.env.EA_DATABASE_USERNAME,
-  password: process.env.EA_DATABASE_PASSWORD,
-  database: process.env.EA_DATABASE_NAME,
+  username: production ? process.env.EA_PROD_DATABASE_USERNAME : process.env.EA_DATABASE_USERNAME,
+  password: production ? process.env.EA_PROD_DATABASE_PASSWORD : process.env.EA_DATABASE_PASSWORD,
+  database: production ? process.env.EA_PROD_DATABASE_NAME : process.env.EA_DATABASE_NAME,
   synchronize: true,
   dropSchema: false,
   logging: true,
