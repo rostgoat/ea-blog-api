f76480227435333f345a744405f65b7de8b5f8cc
Rost Myshkin <rostmyshkin@gmail.com>
2020-03-24 21:25:11 -0700

    removed ormconfig; trying in app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,12 +10,33 @@ import { MulterModule } from '@nestjs/platform-express';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { LikeModule } from './like/like.module';
+const env = require('dotenv')
+env.config()
+
+const production = process.env.NODE_ENV === 'production';
+
 /**
  * Root Module
  */
 @Module({
   imports: [
-    TypeOrmModule.forRoot(),
+    TypeOrmModule.forRoot({
+      name: 'default',
+      url: production ? process.env.EA_PROD_DATABASE_URL : '',
+      type: production ? process.env.EA_PROD_DATABASE_TYPE : process.env.EA_DATABASE_TYPE,
+      host: production ? process.env.EA_PROD_DATABASE_HOST : process.env.EA_DATABASE_HOST,
+      port: 5432,
+      username: production ? process.env.EA_PROD_DATABASE_USERNAME : process.env.EA_DATABASE_USERNAME,
+      password: production ? process.env.EA_PROD_DATABASE_PASSWORD : process.env.EA_DATABASE_PASSWORD,
+      database: production ? process.env.EA_PROD_DATABASE_NAME : process.env.EA_DATABASE_NAME,
+      synchronize: true,
+      dropSchema: false,
+      logging: true,
+      entities: ['/src/**/*.entity.ts', 'dist/**/*.entity.js'],
+      extra: {
+        ssl: true
+      }
+    }),
     ConfigModule.forRoot({ isGlobal: true }),
     UserModule,
     CommentModule,
