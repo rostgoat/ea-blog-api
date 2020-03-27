dc9a862634e1b1d0f09e673d00b00e57c0d4e709
Rost Myshkin <rostmyshkin@gmail.com>
2020-03-27 00:16:25 -0700

    using __dirname

--- src/shared/fileService.ts
+++ src/shared/fileService.ts
@@ -1,16 +1,18 @@
 import { stat, mkdir, promises } from 'fs';
-import * as mkdirp from 'mkdirp'
+import * as mkdirp from 'mkdirp';
+import { join } from 'path';
 
 /**
  * Create new directory if one does not exist in the file system
  * @param dir Directory name
  */
-export const createDir = async (directory) => {
-    try {
-      return await mkdirp(directory);
-    } catch (error) {
-      if (error.code != 'EEXIST') {
-        throw error;
-      }
+export const createDir = async directory => {
+  try {
+    const dirPath = join(__dirname, directory);
+    return await mkdirp(dirPath);
+  } catch (error) {
+    if (error.code != 'EEXIST') {
+      throw error;
     }
-  };
+  }
+};
