d6adf10554a8c618e907925d11c9deca4c5d246d
Rost Myshkin <rostmyshkin@gmail.com>
2020-03-27 02:00:25 -0700

    swagger refactor

--- src/config/swagger/swagger.ts
+++ src/config/swagger/swagger.ts
@@ -1,35 +1,106 @@
-import { DocumentBuilder } from '@nestjs/swagger';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { INestApplication } from '@nestjs/common';
+import { CommentModule } from 'src/comment/comment.module';
+import { PostModule } from 'src/post/post.module';
+import { UserModule } from 'src/user/user.module';
+import { AuthModule } from 'src/auth/auth.module';
+import { LikeModule } from 'src/like/like.module';
+import { PhotoModule } from 'src/photo/photo.module';
 
 /**
  * This file contains all the Swagger API docs for every schema type
  */
-const DESCRIPTION = 'The EA Blog API';
+const DESCRIPTION = 'The Game Bible Blog API';
 const VERSION = '1.0.0';
 
 // User API methods
-export const commonOptions = new DocumentBuilder()
+const commonOptions = new DocumentBuilder()
   .setTitle('All API Methods')
   .setDescription(DESCRIPTION)
   .setVersion(VERSION)
   .build();
 
 // User API methods
-export const usersOptions = new DocumentBuilder()
+const usersOptions = new DocumentBuilder()
   .setTitle('Users')
   .setDescription(DESCRIPTION)
   .setVersion(VERSION)
   .build();
 
 // Blog Posts API methods
-export const postsOptions = new DocumentBuilder()
+const postsOptions = new DocumentBuilder()
   .setTitle('Posts')
   .setDescription(DESCRIPTION)
   .setVersion(VERSION)
   .build();
 
 // Comments API methods
-export const commentsOptions = new DocumentBuilder()
+const commentsOptions = new DocumentBuilder()
   .setTitle('Comments')
   .setDescription(DESCRIPTION)
   .setVersion(VERSION)
   .build();
+
+// Likes API methods
+const likesOptions = new DocumentBuilder()
+  .setTitle('Likes')
+  .setDescription(DESCRIPTION)
+  .setVersion(VERSION)
+  .build();
+
+// Photos API methods
+const photosOptions = new DocumentBuilder()
+  .setTitle('Photos')
+  .setDescription(DESCRIPTION)
+  .setVersion(VERSION)
+  .build();
+
+// Authentication API methods
+const authOptions = new DocumentBuilder()
+  .setTitle('Authentication')
+  .setDescription(DESCRIPTION)
+  .setVersion(VERSION)
+  .build();
+
+/**
+ * Return module and options for Swagger docs
+ * @param route Route
+ */
+const getModuleAndOptions = (route: string) => {
+  switch (route) {
+    case 'common':
+      return { options: commonOptions };
+    case 'users':
+      return { options: usersOptions, mod: UserModule };
+    case 'posts':
+      return { options: postsOptions, mod: PostModule };
+    case 'comments':
+      return { options: commentsOptions, mod: CommentModule };
+    case 'auth':
+      return { options: authOptions, mod: AuthModule };
+    case 'likes':
+      return { options: likesOptions, mod: LikeModule };
+    case 'photos':
+      return { options: photosOptions, mod: PhotoModule };
+  }
+};
+
+/**
+ * Create swagger docs modules
+ *
+ * @param app Nest App
+ * @param route Mdoule Route Name
+ * @param endpoint Swagger API endpoint
+ */
+export const initDocumentation = (
+  endpoint: string,
+  app: INestApplication,
+  route: string,
+) => {
+  const { options, mod } = getModuleAndOptions(route);
+
+  const document = SwaggerModule.createDocument(app, options, {
+    include: [mod] ? [mod] : [],
+  });
+  SwaggerModule.setup(endpoint, app, document);
+};
--- src/main.ts
+++ src/main.ts
@@ -1,53 +1,32 @@
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { SwaggerModule } from '@nestjs/swagger';
 import { Logger } from '@nestjs/common';
+import { initDocumentation } from './config/swagger/swagger';
 import 'reflect-metadata';
-import { UserModule } from './user/user.module';
-import {
-  usersOptions,
-  postsOptions,
-  commentsOptions,
-  commonOptions,
-} from './config/swagger/swagger';
-import { PostModule } from './post/post.module';
-import { CommentModule } from './comment/comment.module';
-import { NestExpressApplication } from '@nestjs/platform-express';
 
 // local dev port
 const port = process.env.PORT || process.env.EA_API_PORT;
+
 /**
  * API entry point
  */
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  // connecting user docs to swagger
-  const commonDocument = SwaggerModule.createDocument(app, commonOptions);
-
-  // connecting user docs to swagger
-  const userDocument = SwaggerModule.createDocument(app, usersOptions, {
-    include: [UserModule],
-  });
-
-  // connecting blog posts docs to swagger
-  const postDocument = SwaggerModule.createDocument(app, postsOptions, {
-    include: [PostModule],
-  });
-
-  // connecting comments docs to swagger
-  const commentDocument = SwaggerModule.createDocument(app, commentsOptions, {
-    include: [CommentModule],
-  });
-
-  // defining docs routes
-  SwaggerModule.setup('api', app, commonDocument);
-  SwaggerModule.setup('api/users', app, userDocument);
-  SwaggerModule.setup('api/posts', app, postDocument);
-  SwaggerModule.setup('api/comments', app, commentDocument);
+  // defining swagger docs routes
+  initDocumentation('api', app, 'common');
+  initDocumentation('api/users', app, 'users');
+  initDocumentation('api/posts', app, 'posts');
+  initDocumentation('api/likes', app, 'likes');
+  initDocumentation('api/auth', app, 'auth');
+  initDocumentation('api/photos', app, 'photos');
+  initDocumentation('api/comments', app, 'comments');
 
   app.enableCors();
+
   await app.listen(port);
+
   Logger.log(`Server running on port http://localhost:${port}`, 'Bootstrap');
 }
 bootstrap();
