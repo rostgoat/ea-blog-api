3be31ebcfee253f7215799be7b93b89914cff3b7
Rost Myshkin <rostmyshkin@gmail.com>
2020-04-06 22:09:25 -0700

    integratio test for user service

---
+++ src/user/user.e2e-spec.ts
@@ -0,0 +1,66 @@
+import { Test, TestingModule } from '@nestjs/testing'
+import { INestApplication } from '@nestjs/common'
+import { AppModule } from '../app.module'
+import { UserModule } from './user.module'
+import { TypeOrmModule, getRepositoryToken } from '@nestjs/typeorm'
+import { Repository, getConnection } from 'typeorm'
+import { User } from './user.entity'
+import supertest = require('supertest')
+import { UserService } from './user.service'
+import { UserController } from './user.controller'
+import { UserCreateDTO } from './user.create.dto'
+
+describe('AppController (e2e)', () => {
+  let app: INestApplication
+  let repository: Repository<User>
+  let service: UserService
+
+  beforeAll(async () => {
+    const module = await Test.createTestingModule({
+      imports: [
+        UserModule,
+        // Use the e2e_test database to run the tests
+        TypeOrmModule.forRoot({
+          type: 'postgres',
+          host: 'localhost',
+          port: 5432,
+          username: 'rm',
+          password: 'root',
+          database: 'ea_games_blog_test',
+          entities: ['./**/*.entity.ts'],
+          synchronize: false,
+        }),
+      ],
+      providers: [UserService],
+      controllers: [UserController],
+    }).compile()
+
+    service = module.get<UserService>(UserService)
+  })
+
+  describe('GET /users', () => {
+    const user: Partial<UserCreateDTO> = {
+      name: 'Mike Test User',
+      email: 'usertest@gmail.com',
+    }
+    it('should create a user is the DB', async () => {
+      const res = await service.add(user)
+      console.log('res', res)
+    })
+  })
+  afterEach(async () => {
+    await repository.query(`DELETE FROM users;`)
+  })
+
+  afterAll(async () => {
+    const connection = getConnection()
+
+    await connection
+      .createQueryBuilder()
+      .delete()
+      .from(User)
+      .execute()
+
+    await connection.close()
+  })
+})
