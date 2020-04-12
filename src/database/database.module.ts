aa30089454b14682e6e40f723c4426a3493e2bb2
Rost <rdavidoff@hotmail.ca>
2020-04-12 00:45:31 -0700

    adding env vars to circleci

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -9,26 +9,21 @@ import { ConfigService } from '../config/config.service'
  */
 @Module({
   imports: [
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('DATABASE_HOST'),
-        port: Number(configService.get('DATABASE_PORT')),
-        username: configService.get('DATABASE_USERNAME'),
-        password: configService.get('DATABASE_PASSWORD'),
-        database: configService.get('DATABASE_NAME'),
-        entities: ['dist/**/*.entity{.ts,.js}'],
-        dropSchema: false,
-        // ! ALWAYS FALSE IN PROD
-        synchronize: !(process.env.NODE_ENV.trim() === 'production'),
-        logging: false,
-        extra: {
-          ssl: true,
-        },
-      }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      dropSchema: false,
+      synchronize: !(process.env.NODE_ENV.trim() === 'production'),
+      logging: false,
+      extra: {
+        ssl: true,
+      },
     }),
   ],
 })
-export class DatabaseModule {}
+export class DatabaseModule { }
